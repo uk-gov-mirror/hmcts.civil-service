612b0dd595af83a307d93bdfa2b8c0ee485d4922
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-04-14 09:59:34 +0100

    Updated test after rebase

--- src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifierTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/notification/handlers/CamundaProcessIdentifierTest.java
@@ -11,7 +11,7 @@ class CamundaProcessIdentifierTest {
     void shouldContainAllExpectedEnumValues() {
         CamundaProcessIdentifier[] values = CamundaProcessIdentifier.values();
 
-        assertThat(values).hasSize(68)
+        assertThat(values).hasSize(71)
             .contains(
                 CamundaProcessIdentifier.ClaimDismissedNotifyParties,
                 CamundaProcessIdentifier.ClaimantConfirmProceedNotifyParties,
@@ -107,6 +107,6 @@ class CamundaProcessIdentifierTest {
 
         assertThat(values)
             .satisfies(v -> assertThat(v[0]).isEqualTo(CamundaProcessIdentifier.ClaimDismissedNotifyParties))
-            .satisfies(v -> assertThat(v[67]).isEqualTo(CamundaProcessIdentifier.DJ_NON_DIVERGENT_NOTIFIER));
+            .satisfies(v -> assertThat(v[70]).isEqualTo(CamundaProcessIdentifier.DJ_NON_DIVERGENT_NOTIFIER));
     }
 }
